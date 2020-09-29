8afdf559ae44ae2116b2ca70c66dceb114f2f565
elmer <baca217@gmail.com>
2020-09-29 18:42:43 +0100

    fixed main call

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import voice_rec
 
 def main():
-    filename = "downSamp.wave"
-    decode_file(filename)
+    filename = "downSamp.wav"
+    voice_rec.decode_file(filename)
 
 if __name__ == "__main__":
     main()
