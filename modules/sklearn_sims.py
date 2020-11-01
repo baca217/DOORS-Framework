03f0042219607243e4a6353069d69decf3711777
elmer <baca217@gmail.com>
2020-11-01 21:26:26 +0000

    adjusted comparison algorithm, multiple version of command now allowed

--- modules/sklearn_sims.py
+++ modules/sklearn_sims.py
@@ -50,54 +50,59 @@ def clean_comp_work(spoken, commands, comTypes):
 
 
 def comp_work(spoken, commands, comTypes):
-    commands.append(spoken) #append spoken for vectorization
     print("------NORMAL-------")
-    #algorithm needs some adjusment
-    cleaned = list(map(clean_string, commands))
-    #print(cleaned)
 
-    vectorizer = CountVectorizer().fit_transform(commands)
-    vectors = vectorizer.toarray()
+    #vectorizer = CountVectorizer().fit_transform(commands)
+    #vectors = vectorizer.toarray()
     bScore = 0 #keeping track of the best score for cosine comparison
-    bComm = ""
+    bComm = "" #best sentence matched
+    bOrig = "" #representation of best sentence
 
     for i in range(len(comTypes)):
-        if comTypes[i] == "exact":
-            if commands[i] in spoken: #exact matches will return immediately
+        if comTypes[i].strip() == "exact":
+            if commands[i][0] in spoken: #exact matches will return immediately
                     return commands[i]
-        if comTypes[i] == "cosine":
-                ret = cosine_sim_vectors(vectors[-1], vectors[i]) #compare spoken vector to command vector
-                print("sentence 1: ", commands[-1])
-                print("sentence 2: ", commands[i])
+        if comTypes[i].strip() == "cosine":
+            tempArr = commands[i] #pulling similar commands
+            tempArr.append(spoken) #adding spoken command to vectorize it
+            vectorizer = CountVectorizer().fit_transform(tempArr) #vectorize
+            vectors = vectorizer.toarray() #turn to array
+            for j in range(len(tempArr)-1):
+                ret = cosine_sim_vectors(vectors[-1], vectors[j]) #compare spoken vector to command vector
+                print("sentence 1: ", tempArr[-1])
+                print("sentence 2: ", tempArr[j])
                 print("similarity: ", ret)
-                print("distance:",Levenshtein.distance(spoken, commands[i]),"\n")
+                print("distance:",Levenshtein.distance(spoken, tempArr[j]),"\n")
                 if ret > .8 and ret > bScore:
                     bScore = ret
-                    bComm = commands[i]
-    return bComm
+                    bComm = tempArr[j]
+                    bOrig = tempArr[0]
+    print("bScore:",bScore,"\nbComm:",bComm,"\nbOrig:",bOrig)
+    exit()
+    return bOrig
 
 
 def compare_command(spoken):
     c_file = open("/home/pi/Documents/DOORS/modules/commands.txt", "r")
     commands = c_file.read()
     commands = commands.split("\n")
-    commands = [x for x in commands if x != ""]
+    commands = [x for x in commands if x != ""] #removing empty entries
     classify = []
-    for x in range(len(commands)): #pulling classification from word
-        temp = commands[x].split()[-1]
-        commands[x] = commands[x].replace(temp, "").strip()
+    for x in range(len(commands)): #pulling classification type from word
+        temp = commands[x].split(",")[-1] #classification is always at the end
+        commands[x] = commands[x].replace(temp, "").strip() #remove classification type
         classify.append(temp)
-    print("commands: ",commands)
-    print("classify: ",classify)
-
+    arrCommands = []
+    for i in commands:
+        arrCommands.append(list(filter(None, i.split(",")))) #splitting command variations, removing empty strings
     if "homie" in spoken:
-        spoken = spoken.replace("homie ",  "")
+        spoken = spoken.replace("homie ",  "").strip()
     else:
         print("homie was not detected! Exiting....")
         exit()
 
-    result = comp_work(spoken, commands, classify)
-    clean_comp_work(spoken, commands, classify)
+    result = comp_work(spoken, arrCommands, classify)
+    #clean_comp_work(spoken, arrCommands, classify)
     print("result:",result)
 
     return spoken, result
