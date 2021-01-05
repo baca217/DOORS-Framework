af82942463cb395dd1a9c8cb2fb9a551399ec000
baca217 <baca217@gmail.com>
2021-01-04 20:40:12 -0700

    fixed and finalized install script

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 MODEL="/modules/model"
 CURDIR=`pwd`
-if [ -d "$MODEL" ] 
+if [ -d "$CURDIR$MODEL" ] 
 then
 	echo "$MODEL installed"
 else
 	echo "$MODEL not installed"
 	echo "installing now"
-	mkdir "$CURDIR
+	mkdir "$CURDIR$MODEL"
+	wget https://alphacephei.com/vosk/models/vosk-model-en-us-daanzu-20200905.zip -P $CURDIR$MODEL #download model
+	unzip $CURDIR$MODEL/vosk*
+	mv vosk* vosk-model #easier rename for moving stuff
+	mv vosk-model/* $CURDIR$MODEL #move stuff to correct directory
+	rm -r vosk-model #rm temp directory
 fi
+#pip3 install vosk
+#pip3 install sklearn
+#pip3 install fuzzywuzzy
+#pip3 install eyed3
+#pip3 install word2number
+#sudo apt-get update
+#sudo apt-get install libgfortran5
+#sudo apt-get install libgfortran3
+#sudo apt-get install libatlas-base-dev
--- modules/vosk_rec.py
+++ modules/vosk_rec.py
@@ -10,54 +10,54 @@ import math
 import random
 
 class Decoder:
-    def __init__(self):
-        model = Model("/home/pi/Documents/DOORS/modules/model")
-        self.rec = KaldiRecognizer(model, 16000)
+	def __init__(self):
+		model = Model("/home/pi/Documents/DOORS/modules/model")
+		self.rec = KaldiRecognizer(model, 16000)
 
-    def decode_file(self, aud_file):
-        SetLogLevel(0)
+	def decode_file(self, aud_file):
+		SetLogLevel(0)
+		sentence = ""
 
-        wf = wave.open(aud_file, "rb")
-        if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-            print ("Audio aud_file must be WAV format mono PCM.")
-            exit (1)
+		wf = wave.open(aud_file, "rb")
+		if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+			print ("Audio aud_file must be WAV format mono PCM.")
+			exit (1)
 
-        results = []
-        
-        while True:
-            data = wf.readframes(4000)
-            if len(data) == 0:
-                break
-            if self.rec.AcceptWaveform(data):
-                results.append(self.rec.Result())
- 
-        #for i in results:
-        #    y = json.loads(i)
-        #    print("---VOSK TEXT---",y["text"])
-        #print("results size", len(results))
-        #print("results:", results)
-        return results
+		results = []
 
-    def decode_stream(self, socket, initData):
-        fname = 'temp.wav'
-        cur = 1
-        obj = wave.open(fname, 'wb')
-        obj.setchannels(1) #mono
-        obj.setsampwidth(2)
-        obj.setframerate(8000)
-        obj.writeframesraw(initData)
-        obj.close
-        results = self.decode_file(fname)
-        print("results "+cur+":"+results)
+		while True:
+			data = wf.readframes(4000)
+			if len(data) == 0:
+				break
+			if self.rec.AcceptWaveform(data):
+				results.append(self.rec.Result())
+
+		for i in results:
+			sentence += json.loads(i)
+		print("results size", len(results))
+		print("sentence:", sentence)
+		return sentence
+
+	def decode_stream(self, socket, initData):
+		fname = 'temp.wav'
+		cur = 1
+		obj = wave.open(fname, 'wb')
+		obj.setchannels(1) #mono
+		obj.setsampwidth(2)
+		obj.setframerate(8000)
+		obj.writeframesraw(initData)
+		obj.close
+		results = self.decode_file(fname)
+		print("results "+cur+":"+results)
 
 
-        while true:
-            obj = wave.open(fname, 'wb')
-            obj.setchannels(1) #mono
-            obj.setsampwidth()
-            obj.setframerate(8000)
-            obj.writeframesraw(socket.read(1024))
-            obj.close
-            results = self.decode_file(fname)
-            print("results "+cur+":"+results)
-            cur += 1
+		while true:
+		    obj = wave.open(fname, 'wb')
+		    obj.setchannels(1) #mono
+		    obj.setsampwidth()
+		    obj.setframerate(8000)
+		    obj.writeframesraw(socket.read(1024))
+		    obj.close
+		    results = self.decode_file(fname)
+		    print("results "+cur+":"+results)
+		    cur += 1
