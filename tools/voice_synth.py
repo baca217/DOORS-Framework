bc8b4741063d66b976175ddbe2fae313070b761d
baca217 <baca217@gmail.com>
2021-03-28 07:05:11 +0100

    added small starting change to voice synth

--- tools/voice_synth.py
+++ tools/voice_synth.py
@@ -14,9 +14,29 @@ class VoiceSynth:
 			comb = ""
 			for i in sentence:
 				comb += i
-			comb = comb.replace("\n", " ")	
+			comb = comb.replace("\n", " ")
 			command = 'espeak \"{}\" --stdout |aplay 2>/dev/null'.format(comb)
 			system(command)
+        def sentToFront(self, sentence): #still working on this
+                SIZE = int(65536/2)
+                #open file for sending
+                f = open("Song.wav", "rb")
+                binaryHeader = f.read(44) #remove .wav header info for raw format
+                # Create a TCP/IP socket
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                # Connect the socket to the port where the server is listening
+                server_address = ('192.168.43.151', 5555)
+                print (sys.stderr, 'connecting to %s port %s' % server_address)
+                sock.connect(server_address)
+                sock.send(b"APCKT\0")
+                size = 1
+                while size > 0:
+                        read = f.read(SIZE)
+                        size = len(read)
+                        print(size)
+                        sock.send(read)
+                sock.close()
 
 	def enable(self):
 		self.enabled = True
